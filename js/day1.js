3edc1d9542633112079eff99d63f1ab7bdfdc9db
hehaosen <307052084@qq.com>
2016-02-02 16:57:26 +0800

    增加了公用的几个方法

--- js/day1.js
+++ js/day1.js
@@ -1,1 +1,9 @@
-alert('进入了第一天');
+var day1 = function () {
+
+    var shadow = new shadowText();
+    shadow.init({
+        text: '我是一个天才',
+        x: 50,
+        y: 50
+    });
+}();
--- js/util.js
+++ js/util.js
@@ -16,6 +16,30 @@ var createJS = function (name) {
 }
 
 /**
+ * 16进制颜色转为RGB格式
+ * @returns array[R,G,B]
+ */
+toColorRgb = function(sColor){
+    if (sColor) {
+        if (sColor.length === 4) {
+            var sColorNew = "#";
+            for(var i = 1; i < 4; i += 1){
+                sColorNew += sColor.slice(i, i+1).concat(sColor.slice(i, i + 1));
+            }
+            sColor = sColorNew;
+        }
+        //处理六位的颜色值
+        var sColorChange = [];
+        for (var i = 1; i < 7; i += 2) {
+            sColorChange.push(parseInt("0x" + sColor.slice(i, i + 2)));
+        }
+        return sColorChange;
+    } else {
+        return sColor;
+    }
+};
+
+/**
  * 获取主舞台节点
  * @returns {Element}
  */
@@ -38,3 +62,39 @@ addEventListener("keyup", function (e) {
     delete keysDown[e.keyCode];
 }, false);
 
+/**
+ * a 继承 b
+ * @param a
+ * @param b
+ */
+
+var extend = function (a, b) {
+    for (var tem in b) {
+        a[tem] = b[tem];
+    }
+}
+/**
+ * 渐变文字
+ */
+var shadowText = function () {
+    var _self = this;
+    this.conf = {
+        stage: getStage(),//主舞台节点
+        text: '默认文字',//显示文字
+        fontSize: 30, //默认字体大小
+        color: '#fffff',//默认文字颜色
+        x: 0,//x坐标
+        y: 0
+    },
+    this.init = function (data) {
+        extend(_self.conf, data);
+        console.log(_self.conf);
+        _self.conf.stage.font = _self.conf.fontSize + 'px 微软雅黑';
+        _self.conf.stage.fillStyle = _self.conf.color;
+        _self.globalAlpha=0.5;
+        _self.conf.stage.fillText(_self.conf.text, _self.conf.x, _self.conf.y);
+    },
+    this.animation = function () {
+
+    }
+}
