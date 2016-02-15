64d4f7959f407be92258f2754606522f4a7cc13b
heartblueflower <heartblueflower@163.com>
2016-02-15 18:00:44 +0800

    require走起

--- js/day1.js
+++ js/day1.js
@@ -1,9 +1,5 @@
 var day1 = function () {
-
-    var shadow = new shadowText();
-    shadow.init({
-        text: '我日2',
-        x: 50,
-        y: 50
-    });
+    createJS("story");
+    story=new Story();
+    story.load("story.xml");
 }();
--- js/main.js
+++ js/main.js
@@ -1,61 +1,6 @@
-/**
- * 引导页面
- */
-
-var lead = function () {
-
-    var bgReady = false;
-    var bgImage = new Image();
-    bgImage.onload = function () {
-        bgReady = true;
-    };
-    bgImage.src = 'images/main/bg.jpg';
-    var choseMenu = 1;
-    var over = 0; // 0 未选择 1.新的开始 2.旧的回忆
-    var alpha = 0;
-
-    // 渲染对象
-    var render = function () {
-        if (bgReady) {
-            $().drawImage(bgImage, 0, 0, 600, 600);
-            $().font = "30px 微软雅黑";
-            $().fillStyle = "red";
-
-            if(choseMenu % 2 == 0) {
-                $().fillText("新的开始", 250, 300);
-                $().fillText("旧的回忆←", 250, 400);
-            } else {
-                $().fillText("新的开始←", 250, 300);
-                $().fillText("旧的回忆", 250, 400);
-            }
-        }
-
-        if (over == 1) {
-            $().fillStyle = 'rgba(181, 25, 25, ' + (alpha++) / 100 + ')';  //填充的颜色
-            $().linewidth = 10;  //边框宽
-            $().fillRect(0, 0, 600, 600);  //填充颜色 x y坐标 宽 高
-        }
-
-    }
-
-    // 主刷新进程
-    var main = function () {
-        render();
-        if (alpha <= 100) {
-            requestAnimationFrame(main);
-        } else {
-            createJS('day1');
-        }
-    };
-
-    KeyBoard.on("up",function(){
-        choseMenu ++
-    });
-    KeyBoard.on("down",function(){
-        choseMenu --
-    });
-    KeyBoard.on("enter",function(){
-        choseMenu % 2 == 0 ? over = 2 : over = 1;
-    });
-    main();
-}();
+require.config({paths:{
+    "lead":"lead"
+}});
+require(['lead'],function(lead){
+    alert(lead.main(1,1));
+});
